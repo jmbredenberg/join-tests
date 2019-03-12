e08b36833e19e277f5dacd578d015015a34e3d13
Jackie Bredenberg <jamb@mit.edu>
2019-03-12 19:00:19 -0400

    create views parse successfully

--- src/lib.rs
+++ src/lib.rs
@@ -9,13 +9,6 @@ use std::path::Path;
 
 mod graph;
 
-fn parse_queryset(queries: Vec<String>) -> (i32, i32) {
-    let (ok, err) = parse_queries(queries);
-
-    println!("Parsing failed: {} queries", err);
-    println!("Parsed successfully: {} queries", ok);
-    (ok, err)
-}
 
 fn test_queries_from_file(f: &Path, name: &str) -> Result<i32, i32> {
     let mut f = File::open(f).unwrap();
@@ -25,59 +18,42 @@ fn test_queries_from_file(f: &Path, name: &str) -> Result<i32, i32> {
     f.read_to_string(&mut s).unwrap();
     let lines: Vec<String> = s
         .lines()
-        .filter(|l| !l.is_empty() && !l.starts_with("#") && !l.starts_with("--") && !l.starts_with("/*"))
+        .filter(|l| {
+            !l.is_empty() && !l.starts_with("#") && !l.starts_with("--") && !l.starts_with("/*")
+        })
+        .map(|l| {
+            if l.starts_with("VIEW") {
+                let v: Vec<&str> = l.splitn(2, ":").collect();
+                format!("CREATE VIEW {} AS{}", &v[0][5..], v[1])
+            }
+            else if l.starts_with("QUERY") {
+                let v: Vec<&str> = l.splitn(2, ":").collect();
+                format!("CREATE VIEW {} AS{}", &v[0][6..], v[1])
+            }
+            else {
+                String::from(l)
+            }
+        })
         .map(|l| {
             if !(l.ends_with("\n") || l.ends_with(";")) {
-                String::from(l) + "\n"
+                l + "\n"
             } else {
-                String::from(l)
+                l
             }
         }).collect();
     println!("Loaded {} {} queries", lines.len(), name);
 
     // Try parsing them all
-    let (ok, err) = parse_queryset(lines);
+    let (ok, err) = parse_queries(lines);
+
+    println!("Parsing failed: {} queries", err);
+    println!("Parsed successfully: {} queries", ok);
 
     if err > 0 {
         return Err(err);
     }
     Ok(ok)
 }
-
-fn parse_file(path: &str) -> (i32, i32) {
-    let mut f = File::open(Path::new(path)).unwrap();
-    let mut s = String::new();
-
-    // Load queries
-    f.read_to_string(&mut s).unwrap();
-    let lines: Vec<&str> = s
-        .lines()
-        .map(str::trim)
-        .filter(|l| {
-            !l.is_empty()
-                && !l.starts_with("#")
-                && !l.starts_with("--")
-                && !l.starts_with("DROP")
-                && !l.starts_with("/*")
-        }).collect();
-    let mut q = String::new();
-    let mut queries = Vec::new();
-    for l in lines {
-        if !l.ends_with(";") {
-            q.push_str(l);
-        } else {
-            // end of query
-            q.push_str(l);
-            queries.push(q.clone());
-            q = String::new();
-        }
-    }
-    println!("Loaded {} table definitions", queries.len());
-
-    // Try parsing them all
-    parse_queryset(queries)
-}
-
 
 
 #[test]
